c7faccde67f520bb3bc80c91772ef0f4aa8af253
Christopher Knop <chris.d.k.knop@gmail.com>
2025-06-02 01:10:22 +0200

    update deps

--- deps.sh
+++ deps.sh
@@ -3,9 +3,7 @@
 DEPS="$PWD/deps"
 
 git clone https://gitlab.com/libeigen/eigen.git
-
 mkdir deps
-
 cd eigen
 cmake . -B build -DCMAKE_INSTALL_PREFIX=$DEPS -DCMAKE_BUILD_TYPE=Release
 cmake --install build
