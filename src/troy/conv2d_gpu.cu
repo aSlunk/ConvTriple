5d8bd67b32913d4290726ef40d82e3de2b1377c7
Christopher Knop <chris.d.k.knop@gmail.com>
2025-10-17 21:59:13 +0200

    setup basics

--- src/troy/conv2d_gpu.cu
+++ src/troy/conv2d_gpu.cu
@@ -1,11 +1,27 @@
-#include "troy/troy.h"
-
 #include "conv2d_gpu.cuh"
+
+#include "troy/troy.h"
 
 namespace TROY {
 
 void conv2d() {
-    std::cout << "TROY\n";
+    using namespace troy;
+    size_t poly_mod = 4096;
+    size_t plain_mod = 1lu << 32;
+    SchemeType scheme = SchemeType::BFV;
+        
+    EncryptionParameters parms(scheme);
+    parms.set_coeff_modulus(CoeffModulus::create(poly_mod, { 60, 40 }).to_vector());
+    parms.set_plain_modulus(plain_mod);
+    parms.set_poly_modulus_degree(poly_mod);
+    HeContextPointer he = HeContext::create(parms, true, SecurityLevel::Classical128);
+
+    BatchEncoder encoder(he);
+    std::cout << utils::device_count() << "\n";
+    if (utils::device_count() > 0) {
+        he->to_device_inplace();
+        encoder.to_device_inplace();
+    }
 }
 
-}
+} // namespace TROY
