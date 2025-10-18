6e2989ef6442f7c8c7e58c388fe32a9415087c08
Christopher Knop <chris.d.k.knop@gmail.com>
2025-10-18 18:27:40 +0200

    basic conv AB2 without R

--- src/client.cpp
+++ src/client.cpp
@@ -6,6 +6,10 @@
 #include <core/hpmpc_interface.hpp>
 #include <core/networks/resnet50.hpp>
 
+#ifdef USE_CONV_CUDA
+#include <troy/conv2d_gpu.cuh>
+#endif
+
 #define PARTY 2
 
 void print_m128i(__m128i var) {
@@ -30,6 +34,13 @@ int main(int argc, char** argv) {
         threads = std::min(strtoul(argv[5], NULL, 10), (size_t)N_THREADS);
 
     int num_triples = 10;
+
+#ifdef USE_CONV_CUDA
+    auto ios = Utils::init_ios<IO::NetIO>(addr, port, 1);
+    TROY::conv2d(ios, PARTY, 1, 3, 230, 230, 3, 3, 64, 1);
+    delete ios[0];
+    delete[] ios;
+#endif
 
     {
         int tmp = 37'996'272;
