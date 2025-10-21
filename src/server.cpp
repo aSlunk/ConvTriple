ee0b20f5bde022bf8c112733f5020ea5bcbe4b19
Christopher Knop <chris.d.k.knop@gmail.com>
2025-10-21 02:34:53 +0200

    fair parameters

--- src/server.cpp
+++ src/server.cpp
@@ -32,7 +32,7 @@ int main(int argc, char** argv) {
 
 #if USE_CONV_CUDA
     auto ios = Utils::init_ios<IO::NetIO>(nullptr, port, 1);
-    TROY::conv2d(ios, PARTY, 1, 3, 230, 230, 7, 7, 64, 2, 0);
+    TROY::conv2d(ios, PARTY, batchSize, 3, 230, 230, 7, 7, 64, 1, 0);
     // TROY::conv2d(ios, PARTY, 1, 1, 5, 5, 3, 3, 1, 1, 0);
     delete ios[0];
     delete[] ios;
@@ -145,7 +145,7 @@ int main(int argc, char** argv) {
             .fw        = 7,
             .fh        = 7,
             .n_filters = 64,
-            .stride    = 2,
+            .stride    = 1,
             .padding   = 0,
         };
 
