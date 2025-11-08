370476dd3222c9b9689b6a807e2b886fc9ef4a67
Christopher Knop <chris.d.k.knop@gmail.com>
2025-11-08 20:04:31 +0100

    minor refactoring

--- src/include/gemini/core/util/ThreadPool.h
+++ src/include/gemini/core/util/ThreadPool.h
@@ -63,20 +63,18 @@ inline ThreadPool::ThreadPool(size_t threads) : stop(false) {
     workers.reserve(threads);
     for (size_t i = 0; i < threads; ++i)
         workers.emplace_back([this] {
+            std::unique_lock<std::mutex> lock(this->queue_mutex);
             for (;;) {
                 std::function<void()> task;
+                this->condition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
+                if (this->stop && this->tasks.empty())
+                    return;
+                task = std::move(this->tasks.front());
+                this->tasks.pop();
 
-                {
-                    std::unique_lock<std::mutex> lock(this->queue_mutex);
-                    this->condition.wait(lock,
-                                         [this] { return this->stop || !this->tasks.empty(); });
-                    if (this->stop && this->tasks.empty())
-                        return;
-                    task = std::move(this->tasks.front());
-                    this->tasks.pop();
-                }
-
+                lock.unlock();
                 task();
+                lock.lock();
             }
         });
 }
