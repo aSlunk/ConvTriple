8e910071c576d134f095f5ac07f65c0943b99875
Christopher Knop <chris.d.k.knop@gmail.com>
2025-11-17 21:51:34 +0100

    test multithread multiplexer

--- src/core/hpmpc_interface.cpp
+++ src/core/hpmpc_interface.cpp
@@ -697,12 +697,20 @@ void do_multiplex(int num_input, int party, const std::string& ip, int port, int
     uint64_t* x  = new uint64_t[num_input];
     uint64_t* y  = new uint64_t[num_input];
 
-    for (int i = 0; i < num_input; ++i) {
-        sel[i] = party == 1 ? i % 2 : (i % 2) ^ 1;
-        x[i]   = i;
-    }
+    auto func = [&] (int wid, size_t start, size_t end) -> Code {
+        if (start >= end) return Code::OK;
 
-    Aux::multiplexer(keys.get_otpack(0), party, sel, x, y, num_input, bitlen, bitlen);
+        for (size_t i = start; i < end; ++i) {
+            sel[i] = party == 1 ? i % 2 : (i % 2) ^ 1;
+            x[i]   = i;
+        }
+
+        Aux::multiplexer(keys.get_otpack(wid), party, sel + start, x + start, y + start, end - start, bitlen, bitlen);
+        return Code::OK;
+    };
+
+    gemini::ThreadPool tpool(threads);
+    gemini::LaunchWorks(tpool, num_input, func);
 
     Utils::log(Utils::Level::INFO, "P", party - 1,
                ": multiplex time[s]: ", Utils::to_sec(Utils::time_diff(start)));
