39b6fba9b844500f56b376725250280cc18265c5
Christopher Knop <chris.d.k.knop@gmail.com>
2025-12-11 01:31:38 +0100

    fixed gpu deps

--- deps.sh
+++ deps.sh
@@ -65,7 +65,7 @@ if [[ "$1" = "-gpu" ]]; then
     cmake -B build . -DCMAKE_INSTALL_PREFIX=$BUILD_DIR \
         -DCMAKE_CUDA_ARCHITECTURES="75;80;89" -DCMAKE_BUILD_TYPE=$BUILD_MODE \
         -DCMAKE_PREFIX_PATH=$BUILD_DIR -DTROY_PYBIND=OFF -DTROY_TEST=ON \
-        -DTROY_BENCH=ON -DTROY_EXAMPLES=ON \
+        -DTROY_BENCH=ON -DTROY_EXAMPLES=ON
     cmake --build build -t install -j
     # export CMAKE_GENERATOR="Unix Makefiles"
     # bash scripts/build.sh -install -prefix=$BUILD_DIR
